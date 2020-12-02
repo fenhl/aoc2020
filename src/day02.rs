d889473a26a69124ddf33d4be173e6a61b4dfcd0
Fenhl <fenhl@fenhl.net>
2020-12-02 05:28:36 +0000

    Day 2

---
+++ src/day02.rs
@@ -0,0 +1,33 @@
+use {
+    std::ops::RangeInclusive,
+    lazy_static::lazy_static,
+    regex::Regex,
+};
+
+lazy_static! {
+    static ref LINE_REGEX: Regex = Regex::new("^([0-9]+)-([0-9]+) (.?): (.*)$").unwrap();
+}
+
+#[aoc_generator(day2)]
+fn gen(input: &str) -> Vec<(RangeInclusive<usize>, char, String)> {
+    input.lines()
+        .map(|line| {
+            let captures = LINE_REGEX.captures(line).unwrap();
+            (captures[1].parse().unwrap()..=captures[2].parse().unwrap(), captures[3].parse().unwrap(), captures[4].to_owned())
+        })
+        .collect()
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &[(RangeInclusive<usize>, char, String)]) -> usize {
+    input.iter()
+        .filter(|(range, letter, password)| range.contains(&password.matches(*letter).count()))
+        .count()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &[(RangeInclusive<usize>, char, String)]) -> usize {
+    input.iter()
+        .filter(|(range, letter, password)| (password.chars().nth(*range.start() - 1).unwrap() == *letter) ^ (password.chars().nth(*range.end() - 1).unwrap() == *letter))
+        .count()
+}
